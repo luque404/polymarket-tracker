5e32d5684ffd5d77299af23659d522eb68429157
luque404 <martinluque29@hotmail.com>
2026-03-25 10:25:08 +0100

    app.py

--- app.py
+++ app.py
@@ -172,3 +172,7 @@ def get_markets():
                     prices = json.loads(prices)
                 if prices:
                     prob = round(float(prices[0]) *
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
